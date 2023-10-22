b3518091ab4d03afc6de0e07ea54de31cb4edf79
diogo-mine <diogo_mine@hotmail.com>
2023-10-22 02:55:20 -0300

    Initial commit

---
+++ src/main/java/com/example/desafioapicriacaocharacter/DesafioApiCriacaoCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioapicriacaocharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiCriacaoCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiCriacaoCharacterApplication.class, args);
+	}
+
+}
